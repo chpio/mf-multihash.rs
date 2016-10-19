874c685c9100f2aa41ce7fc910e84df9e13045ad
Thomas Heck <t@b128.net>
2016-10-19 14:02:54 +0200

    overhaul
    - store hashes on stack
    - use rust-crypto instead of sodium
    - impl all algos defined by multihash spec
    - rm HashTypes

--- src/lib.rs
+++ src/lib.rs
@@ -19,112 +19,116 @@ missing_debug_implementations)]
 /// ! in Rust.
 /// Representation of a Multiaddr.
 
-extern crate sodiumoxide;
+extern crate crypto;
 
-use sodiumoxide::crypto::hash::{sha256, sha512};
-use std::io;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+use crypto::sha2::{Sha256, Sha512};
+use crypto::sha3::Sha3;
+use crypto::blake2b::Blake2b;
+use crypto::blake2s::Blake2s;
 
-mod hashes;
-pub use hashes::*;
-
-
-
-/// Encodes data into a multihash.
-///
-/// The returned data is raw bytes.  To make is more human-friendly, you can encode it (hex,
-/// base58, base64, etc).
-///
-/// # Errors
-///
-/// Will return an error if the specified hash type is not supported.  See the docs for `HashTypes`
-/// to see what is supported.
-///
-/// # Examples
-///
-/// ```
-/// use multihash::{encode, HashTypes};
-///
-/// assert_eq!(
-///     encode(HashTypes::SHA2256, "hello world".as_bytes()).unwrap(),
-///     vec![18, 32, 185, 77, 39, 185, 147, 77, 62, 8, 165, 46, 82, 215, 218, 125, 171, 250, 196,
-///     132, 239, 227, 122, 83, 128, 238, 144, 136, 247, 172, 226, 239, 205, 233]
-/// );
-/// ```
-///
-pub fn encode(wanttype: HashTypes, input: &[u8]) -> io::Result<Vec<u8>> {
-    let digest: Vec<u8> = match wanttype {
-        HashTypes::SHA2256 => sha256::hash(input).as_ref().to_owned(),
-        HashTypes::SHA2512 => sha512::hash(input).as_ref().to_owned(),
-        _ => return Err(io::Error::new(io::ErrorKind::Other, "Unsupported hash type")),
-    };
-
-    let mut bytes = Vec::with_capacity(digest.len() + 2);
-
-    bytes.push(wanttype.code());
-    bytes.push(digest.len() as u8);
-    bytes.extend(digest);
-
-    Ok(bytes)
-}
-
-/// Decodes bytes into a multihash
-///
-/// # Errors
-///
-/// Returns an error if the bytes are not a valid multihash.
-///
-/// # Examples
-///
-/// ```
-/// use multihash::{decode, HashTypes, Multihash};
-///
-/// // use the data from the `encode` example
-/// let data = vec![18, 32, 185, 77, 39, 185, 147, 77, 62, 8, 165, 46, 82, 215, 218,
-/// 125, 171, 250, 196, 132, 239, 227, 122, 83, 128, 238, 144, 136, 247, 172, 226, 239, 205, 233];
-///
-/// assert_eq!(
-///     decode(&data).unwrap(),
-///     Multihash {
-///         alg: HashTypes::SHA2256,
-///         digest: data[2..].to_vec()
-///     }
-/// );
-/// ```
-///
-pub fn decode(input: &[u8]) -> io::Result<Multihash> {
-
-    let code = input[0];
-
-    match HashTypes::from_code(code) {
-        Some(alg) => {
-            let hash_len = alg.size() as usize;
-            // length of input should be exactly hash_len + 2
-            if input.len() != hash_len + 2 {
-                Err(io::Error::new(io::ErrorKind::Other,
-                                   format!("Bad input length.  Expected {}, found {}",
-                                           hash_len + 2,
-                                           input.len())))
-            } else {
-                Ok(Multihash {
-                    alg: alg,
-                    digest: input[2..].to_vec(),
-                })
-            }
+macro_rules! impl_multihash {
+    ($($name:ident, $name_hr:expr, $name_lc:ident, $code:expr, $size:expr, $hasher:expr;)*) => {
+        /// Represents a valid multihash, by associating the hash algorithm with the data
+//        #[derive(PartialEq, Eq, Clone, Debug)]
+        pub enum Multihash {
+            $(
+                $name([u8; $size])
+            ),*
         }
-        None => Err(io::Error::new(io::ErrorKind::Other, format!("Unkown code {:?}", code))),
+
+        impl Multihash {
+            /// Converts the Multihash into bytes
+            pub fn to_bytes(&self) -> Vec<u8> {
+                match self {
+                    $(
+                        &Multihash::$name(ref hash) => {
+                            let mut b = Vec::with_capacity(2 + $size);
+                            b.push($code);
+                            b.push($size);
+                            b.extend_from_slice(hash);
+                            b
+                        }
+                    ),*
+                }
+            }
+
+            /// Converts bytes into a Multihash
+            pub fn from_bytes(input: &[u8]) -> Option<Multihash> {
+                match input[0] {
+                    $(
+                        $code => {
+                            let mut buf: [u8; $size] = [0; $size];
+                            if input[1] != $size || input.len() != $size + 2 {
+                                panic!("invalid hash size");
+                            }
+                            for (&i, b) in input[2..].iter().zip(buf.iter_mut()) {
+                                *b = i;
+                            }
+                            Some(Multihash::$name(buf))
+                        }
+                    ),*
+                    _ => None,
+                }
+            }
+
+            /// Returns a slice containing the hash data
+            pub fn hash(&self) -> &[u8] {
+                match self {
+                    $(
+                        &Multihash::$name(ref hash) => hash
+                    ),*
+                }
+            }
+
+            /// Returns the size of the hash data
+            pub fn size(&self) -> u8 {
+                match self {
+                    $(
+                        &Multihash::$name(_) => $size
+                    ),*
+                }
+            }
+
+            /// Returns the human readable name of the hash algorithm
+            pub fn name(&self) -> &str {
+                match self {
+                    $(
+                        &Multihash::$name(_) => $name_hr
+                    ),*
+                }
+            }
+
+            $(
+                /// Hashes the input by the designated algorithm. Returns a
+                /// Multihash containing the hashed data.
+                pub fn $name_lc(input: &[u8]) -> Multihash {
+                    let mut buf: [u8; $size] = [0; $size];
+                    let mut hasher = $hasher;
+                    hasher.input(input);
+                    hasher.result(&mut buf);
+                    Multihash::$name(buf)
+                }
+            )*
+        }
     }
 }
 
-/// Represents a valid multihash, by associating the hash algorithm with the data
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct Multihash {
-    pub alg: HashTypes,
-    pub digest: Vec<u8>,
-}
+impl_multihash! {
+    SHA1, "SHA1", sha1, 0x11, 20, Sha1::new();
 
-/// Convert bytes to a hex representation
-pub fn to_hex(bytes: &[u8]) -> String {
-    bytes.iter()
-        .map(|x| format!("{:02x}", x))
-        .collect()
+    SHA2256, "SHA2-256", sha2_256, 0x12, 32, Sha256::new();
+    SHA2512, "SHA2-512", sha2_512, 0x13, 64, Sha512::new();
+
+    SHA3224, "SHA3-224", sha3_224, 0x17, 28, Sha3::sha3_224();
+    SHA3256, "SHA3-256", sha3_256, 0x16, 32, Sha3::sha3_256();
+    SHA3384, "SHA3-384", sha3_384, 0x15, 48, Sha3::sha3_384();
+    SHA3512, "SHA3-512", sha3_512, 0x14, 64, Sha3::sha3_512();
+
+    SHAKE128, "SHAKE-128", shake_128, 0x18, 16, Sha3::shake_128();
+    SHAKE256, "SHAKE-256", shake_256, 0x19, 32, Sha3::shake_256();
+
+    BLAKE2B, "BLAKE2B", blake2b, 0x40, 64, Blake2b::new(64);
+    BLAKE2S, "BLAKE2S", blake2s, 0x41, 32, Blake2s::new(32);
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,6 @@
 extern crate multihash;
 
-use multihash::*;
+use multihash::Multihash;
 
 /// Helper function to convert a hex-encoded byte array back into a bytearray
 fn hex_to_bytes(s: &str) -> Vec<u8> {
@@ -11,37 +11,37 @@ fn hex_to_bytes(s: &str) -> Vec<u8> {
         c += 2;
     }
     v
-
 }
 
 #[test]
-fn multihash_encode () {
+fn multihash_encode() {
     assert_eq!(
-        encode(HashTypes::SHA2256, "helloworld".as_bytes()).unwrap(),
-        hex_to_bytes("1220936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af")
+        Multihash::sha2_256("helloworld".as_bytes()).to_bytes(),
+        hex_to_bytes("1220936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af").as_slice()
     );
     assert_eq!(
-        encode(HashTypes::SHA2256, "beep boop".as_bytes()).unwrap(),
-        hex_to_bytes("122090ea688e275d580567325032492b597bc77221c62493e76330b85ddda191ef7c")
+        Multihash::sha2_256("beep boop".as_bytes()).to_bytes(),
+        hex_to_bytes("122090ea688e275d580567325032492b597bc77221c62493e76330b85ddda191ef7c").as_slice()
     );
     assert_eq!(
-        encode(HashTypes::SHA2512, "hello world".as_bytes()).unwrap(),
-        hex_to_bytes("1340309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f")
+        Multihash::sha2_512("hello world".as_bytes()).to_bytes(),
+        hex_to_bytes("1340309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f").as_slice()
     );
-
 }
 
 #[test]
-fn multihash_decode () {
-    let hash: Vec<u8> = encode(HashTypes::SHA2256, "helloworld".as_bytes()).unwrap();
-    assert_eq!(
-        decode(&hash).unwrap().alg,
-        HashTypes::SHA2256
-    )
+fn multihash_decode() {
+    let hash = Multihash::sha2_256("helloworld".as_bytes());
+    assert!(match hash {
+        Multihash::SHA2256(_) => true,
+        _ => false,
+    })
 }
 
 #[test]
-fn hash_types () {
-    assert_eq!(HashTypes::SHA2256.size(), 32);
-    assert_eq!(HashTypes::SHA2256.name(), "SHA2-256");
+fn hash_types() {
+    let hash = Multihash::sha2_256(&[]);
+    
+    assert_eq!(hash.size(), 32);
+    assert_eq!(hash.name(), "SHA2-256");
 }
