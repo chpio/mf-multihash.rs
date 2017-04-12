516f97fe6f680e7a1f05c08de38f1c7a0adc9f7f
Thomas Heck <t@b128.net>
2017-04-12 13:45:57 +0200

    + hashing tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,8 @@
 extern crate multihash;
+extern crate ring;
 
 use multihash::{Multihash, HashAlgo};
+use ring::test;
 
 /// Helper function to convert a hex-encoded byte array back into a bytearray
 fn hex_to_bytes(s: &str) -> Vec<u8> {
@@ -14,37 +16,32 @@ fn hex_to_bytes(s: &str) -> Vec<u8> {
 }
 
 #[test]
-fn multihash_serialize() {
-    let mut out = Vec::new();
-    HashAlgo::SHA2256.hash("helloworld".as_bytes()).to_bytes(&mut out).unwrap();
-    assert_eq!(
-        out,
-        hex_to_bytes("1220936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af").as_slice()
-    );
+fn hashing() {
+    test::from_file("./tests/hashing.txt", |_, t| {
+        let algo = t.consume_string("algo");
+        let len = t.consume_usize("len");
+        let input = t.consume_bytes("input");
+        let output = t.consume_bytes("output");
 
-    let mut out = Vec::new();
-    HashAlgo::SHA2256.hash("beep boop".as_bytes()).to_bytes(&mut out).unwrap();
-    assert_eq!(
-        out,
-        hex_to_bytes("122090ea688e275d580567325032492b597bc77221c62493e76330b85ddda191ef7c").as_slice()
-    );
+        let algo = HashAlgo::from_name(algo).unwrap();
+        let mut config = algo.config();
+        if len != 0 {
+            config = config.set_len(len);
+        }
+        let mh = config.hash(&input);
+        let mut out = Vec::new();
+        mh.to_bytes(&mut out).unwrap();
+        assert_eq!(output, out);
 
-    let mut out = Vec::new();
-    HashAlgo::SHA2512.hash("hello world".as_bytes()).to_bytes(&mut out).unwrap();
-    assert_eq!(
-        out,
-        hex_to_bytes("1340309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f").as_slice()
-    );
+        Ok(())
+    });
 }
 
 #[test]
 fn multihash_deserialize() {
     let buf = hex_to_bytes("1220936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af0000");
     let (hash, slice) = Multihash::from_bytes(buf.as_slice()).unwrap();
-    assert!(match hash {
-        Multihash::SHA2256(_) => true,
-        _ => false,
-    });
+    assert_eq!(hash.algo(), Some(HashAlgo::SHA2256));
     assert_eq!(
         hash.hash(),
         hex_to_bytes("936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af").as_slice()
@@ -53,12 +50,4 @@ fn multihash_deserialize() {
         slice,
         hex_to_bytes("0000").as_slice()
     );
-}
-
-#[test]
-fn hash_types() {
-    let hash = HashAlgo::SHA2256.hash(&[]);
-
-    assert_eq!(hash.len(), 32);
-    assert_eq!(hash.name(), "SHA2-256");
 }
