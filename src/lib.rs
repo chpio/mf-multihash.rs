ca3ac1006c27169b07adf8a4ad94402b47ca2df7
Thomas Heck <t@b128.net>
2017-04-12 10:30:30 +0200

    - linting

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-// For explanation of lint checks, run `rustc -W help`
-// This is adapted from
-// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
-#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
-unknown_crate_types, warnings)]
-#![deny(deprecated, improper_ctypes, //missing_docs,
-non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
-private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
-unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
-unused_comparisons, unused_features, unused_parens, while_true)]
-#![warn(trivial_casts, unused_extern_crates, unused_import_braces,
-unused_qualifications, unused_results, variant_size_differences)]
-#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
-missing_debug_implementations)]
-
 /// ! # multihash
 /// !
 /// ! Implementation of [multihash](https://github.com/jbenet/multihash)
